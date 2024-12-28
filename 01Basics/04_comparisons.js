8bb98669c2fad582388842f42590ae5a93904d5d
Om Mahalle <mahalleom794@gmail.com>
2024-12-28 14:26:32 +0000

    Summary editing

--- 01Basics/04_comparisons.js
+++ 01Basics/04_comparisons.js
@@ -1,1 +1,23 @@
-console.log()
+/*Simple Comparisons */
+
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2!=1)
+// console.log(2==1)
+
+
+/*javascript directly converts "2" into a number and then check 
+so value comes out to be comparisons between numbers*/
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/*the assignment operator like > < >= =< converts null to 0 and then compare */
+console.log(null > 0);
+console.log(null >= 0); // true
+
+/* dont convert null value */
+console.log(null == 0);
+
+// "===" checks datatype and value also
+console.log("2" === 2)
