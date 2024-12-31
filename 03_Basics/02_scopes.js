b009bebb1485dab3957040a540b7229e1aae7a3f
Om Mahalle <mahalleom794@gmail.com>
2024-12-31 11:53:00 +0000

    global and local scope

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,13 @@
+/*******  SCOPES     *******/
+
+//Two type of scopes 
+//1. Global Scope 2. Block Scope 
+
+var c = 3 // Don't use its block and global scope is not verified
+
+
+let a = 20
+const b = 2
+
+/**There is diff between scopes in browser and codse environments like vscode**/
+
