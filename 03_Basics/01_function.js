efe07b12e47af0a4675f0aa859e899b5c67e64a3
Om Mahalle <mahalleom794@gmail.com>
2024-12-31 11:11:09 +0000

    functions and parameters

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,13 @@
+function myName(){
+    return "love"
+}
+
+const result = myName()
+console.log(result)
+
+
+function addNumber(num1, num2){//parameters included
+    console.log(`Addition is ${num1+num2}`)
+}
+
+addNumber() //arguments
