92864de7071a9aba08f0fa711b9baba6064f7ffb
Om Mahalle <mahalleom794@gmail.com>
2025-01-01 05:21:17 +0000

    Control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,21 @@
+
+const month = "january"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
