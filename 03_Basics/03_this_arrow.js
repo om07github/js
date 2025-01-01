fc2d7d95b3bbb68a7bb1ecbfbf6d5a955b04f6da
Om Mahalle <mahalleom794@gmail.com>
2025-01-01 03:29:14 +0000

    this and arrow function

--- 03_Basics/03_this_arrow.js
+++ 03_Basics/03_this_arrow.js
@@ -4,7 +4,7 @@ const user = {
     price : 999,
 
     welcomeMessage : function (){
-        console.log(`${this.username}, welcpome to website`);\
+        console.log(`${this.username}, welcpome to website`);
         console.log(this); // Gives all the value in the current context
         
     }
@@ -17,5 +17,20 @@ user.welcomeMessage();
 
 ////
 
-if we use
+// if we use this in browser it gives ouput as a windows cause it is in outer scopoe
 console.log(this) // output `{}`
+
+
+/*** Arrow Function***/
+const addTwo = (num1, num2) => {
+    return num1+num2
+}
+
+if there  is only 1 line inside a function
+const addTwo = (num1,num2) => num1 + num2;
+const addTwo = (num1,num2) => (num1 + num2); // CAn be use like this
+
+const addTwo = (num1, num2) => ({username: "hitesh"}) // object can be ue like this only
+
+
+
