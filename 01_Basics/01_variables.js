ac68b88120b9bf976c396aceffc7b256cb774a55
Om Mahalle <mahalleom794@gmail.com>
2025-01-04 15:32:16 +0000

    hoisting

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,3 +1,20 @@
+// Hoisting is JavaScript's default behavior of moving declarations (variables, functions, and classes) to the top of their scope (script or function) during the compilation phase.
+// Only declarations are hoisted, not initializations or assignments.
+// 1. var Variables
+// var declarations are hoisted to the top of their function or global scope and initialized with undefined.
+// 2. let and const Variables
+// Both let and const are hoisted, but they are placed in a temporal dead zone (TDZ) from the start of the block until the declaration is encountered.
+// Accessing them before declaration results in a ReferenceError.
+
+// Hoisting with Functions
+// 1. Function Declarations
+// Function declarations are hoisted entirely, meaning you can call the function before it is defined in the code.
+// 2. Function Expressions
+// Function expressions are not hoisted like declarations because they are assigned to variables.
+// If the variable is declared with var, it is hoisted and initialized with undefined, resulting in a TypeError when the function is called.
+
+
+
 const accountId = 1345
 let accountEmail = "om@gmail.com"
 var accountPassword = "12345"
